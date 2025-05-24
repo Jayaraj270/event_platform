40e79035ca87a464fb85db3684626ef6d8e63db0
Jayaraj270 <jayaraj4258@email.com>
2025-05-24 06:29:16 +0530

    Your descriptive commit message

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,51 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
-});
- 
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
+  // Pass public and ignored routes here
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+  ]
 };
+
+
+
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+
+// export const config = {
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     '/(api|trpc)(.*)',
+//   ],
+// };
  
