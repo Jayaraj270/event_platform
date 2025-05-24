8b50bb687376d7a569a6caedfa4e4514dbb1914c
Jayaraj270 <jayaraj4258@email.com>
2025-05-24 07:30:34 +0530

    Update Footer component and other changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'TECH-X',
+  description: 'TECH-X is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
